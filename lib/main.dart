5b6a21a556aa7110e50a712423d20fa2743490d5
Lc <641229781@qq.com>
2021-02-25 17:02:19 +0800

    start sliver

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'demo/bottom_navigation_bar_demo.dart';
 import 'demo/basic_demo.dart';
 import 'demo/layout_demo.dart';
 import 'demo/view_demo.dart';
+import 'demo/sliver_demo.dart';
 
 void main() => runApp(App());
 
@@ -15,7 +16,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: SliverDemo(),
       theme: ThemeData(
         primarySwatch: Colors.yellow,
         //设置按下的颜色
